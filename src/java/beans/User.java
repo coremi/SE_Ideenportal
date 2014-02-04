ff91b808a250c55daf01f8dd4cf0cb6e4ff2ef3f
kdm1986 <kdm1986.mail@gmail.com>
2014-02-04 02:53:29 +0100

    read mitarbeiter from database, if username already exists, else create new mitarbeiter

--- src/java/beans/User.java
+++ src/java/beans/User.java
@@ -6,9 +6,16 @@
 
 package beans;
 
+import java.io.Serializable;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Root;
 import model.Mitarbeiter;
 
 /**
@@ -17,7 +24,7 @@ import model.Mitarbeiter;
  */
 @ManagedBean(name = "User")
 @SessionScoped
-public class User {
+public class User implements Serializable {
     String username;
     Mitarbeiter mitarbeiter;
     
@@ -26,12 +33,36 @@ public class User {
      */
     public User() {
         this.username = FacesContext.getCurrentInstance().getExternalContext().getRemoteUser();
-        
-        // get mitarbeiter with username=username
+        setMitarbeiter();
+    }
+    
+    private void setMitarbeiter() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("LightsPU");
+        EntityManager em = emf.createEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        javax.persistence.criteria.CriteriaQuery cq = cb.createQuery();
+        Root<Mitarbeiter> root = cq.from(Mitarbeiter.class);
+        cq.select(root);
+        //sql query where username == username
+        cq.where(cb.equal(root.get("username"), this.username));
+        //liste mit ergebnissen
+        List<Mitarbeiter> list = em.createQuery(cq).getResultList();
+        if (list.isEmpty()) { 
+            //hier muss der neue mitarbeiter iwie mit controller/facade/was weiß ich verbunden werden
+            //hab hier wild mit MAController.create() rumprobiert, hat aber alles nicht geklappt iwie
+            this.mitarbeiter = new Mitarbeiter();
+        } else {
+            //setzt mitarbeiter auf die daten aus der db
+            this.mitarbeiter = list.get(0);
+        }      
     }
     
     public String getUsername() {
         return this.username;
+    }
+    
+    public Mitarbeiter getMitarbeiter() {
+        return this.mitarbeiter;
     }
     
     public void logout() {
